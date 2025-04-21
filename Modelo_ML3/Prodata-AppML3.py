0dcb87ac1ce80286bf48e4031100f236c71d2f07
Joaquin Rubiolo <106486560+joarubiolo@users.noreply.github.com>
2025-04-21 12:02:09 -0300

    Update Prodata-AppML3.py

--- Modelo_ML3/Prodata-AppML3.py
+++ Modelo_ML3/Prodata-AppML3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 
 st.title("Pro-Data: Anlisis de mercado gastronomico")
